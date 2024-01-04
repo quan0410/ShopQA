75d996692297ab80dafdf71ac59330acd863a607
Quân <quan4102000@gmail.com>
2024-01-04 21:22:28 +0700

    update

--- resources/views/admin/layouts/nav.blade.php
+++ resources/views/admin/layouts/nav.blade.php
@@ -25,7 +25,12 @@
             <li class="nav-item navbar-dropdown dropdown-user dropdown">
                 <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
                     <div class="avatar avatar-online">
-                        <img src="../assets/img/avatars/1.png" alt="" class="w-px-40 h-auto rounded-circle">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor"
+                             class="bi bi-person-circle" viewBox="0 0 16 16">
+                            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+                            <path fill-rule="evenodd"
+                                  d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+                        </svg>
                     </div>
                 </a>
                 <ul class="dropdown-menu dropdown-menu-end">
@@ -34,11 +39,16 @@
                             <div class="d-flex">
                                 <div class="flex-shrink-0 me-3">
                                     <div class="avatar avatar-online">
-                                        <img src="../assets/img/avatars/1.png" alt="" class="w-px-40 h-auto rounded-circle">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40"
+                                             fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                                            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+                                            <path fill-rule="evenodd"
+                                                  d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+                                        </svg>
                                     </div>
                                 </div>
                                 <div class="flex-grow-1">
-                                    <span class="fw-semibold d-block">John Doe</span>
+                                    <span class="fw-semibold d-block">{{\Illuminate\Support\Facades\Auth::user()->name}}</span>
                                     <small class="text-muted">Admin</small>
                                 </div>
                             </div>
@@ -47,30 +57,30 @@
                     <li>
                         <div class="dropdown-divider"></div>
                     </li>
-                    <li>
-                        <a class="dropdown-item" href="#">
-                            <i class="bx bx-user me-2"></i>
-                            <span class="align-middle">My Profile</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a class="dropdown-item" href="#">
-                            <i class="bx bx-cog me-2"></i>
-                            <span class="align-middle">Settings</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a class="dropdown-item" href="#">
-                        <span class="d-flex align-items-center align-middle">
-                          <i class="flex-shrink-0 bx bx-credit-card me-2"></i>
-                          <span class="flex-grow-1 align-middle">Billing</span>
-                          <span class="flex-shrink-0 badge badge-center rounded-pill bg-danger w-px-20 h-px-20">4</span>
-                        </span>
-                        </a>
-                    </li>
-                    <li>
-                        <div class="dropdown-divider"></div>
-                    </li>
+{{--                    <li>--}}
+{{--                        <a class="dropdown-item" href="#">--}}
+{{--                            <i class="bx bx-user me-2"></i>--}}
+{{--                            <span class="align-middle">My Profile</span>--}}
+{{--                        </a>--}}
+{{--                    </li>--}}
+{{--                    <li>--}}
+{{--                        <a class="dropdown-item" href="#">--}}
+{{--                            <i class="bx bx-cog me-2"></i>--}}
+{{--                            <span class="align-middle">Settings</span>--}}
+{{--                        </a>--}}
+{{--                    </li>--}}
+{{--                    <li>--}}
+{{--                        <a class="dropdown-item" href="#">--}}
+{{--                        <span class="d-flex align-items-center align-middle">--}}
+{{--                          <i class="flex-shrink-0 bx bx-credit-card me-2"></i>--}}
+{{--                          <span class="flex-grow-1 align-middle">Billing</span>--}}
+{{--                          <span class="flex-shrink-0 badge badge-center rounded-pill bg-danger w-px-20 h-px-20">4</span>--}}
+{{--                        </span>--}}
+{{--                        </a>--}}
+{{--                    </li>--}}
+{{--                    <li>--}}
+{{--                        <div class="dropdown-divider"></div>--}}
+{{--                    </li>--}}
                     <li>
                         <a class="dropdown-item" href="#" onclick="event.preventDefault();
                                                      document.getElementById('form-logout').submit();">
--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -265,36 +265,18 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-lg-4 col-md-6 col-sm-6">
-                <div class="blog__item">
-                    <div class="blog__item__pic set-bg" data-setbg="front/img/blog/blog-1.jpg"></div>
-                    <div class="blog__item__text">
-                        <span><img src="front/img/icon/calendar.png" alt=""> 16 February 2020</span>
-                        <h5>What Curling Irons Are The Best Ones</h5>
-                        <a href="#">Read More</a>
+            @foreach($blogs as $blog)
+                <div class="col-lg-4 col-md-6 col-sm-6">
+                    <div class="blog__item">
+                        <div class="blog__item__pic set-bg" data-setbg="{{asset(Storage::Url($blog->image))}}"></div>
+                        <div class="blog__item__text">
+                            <span><img src="front/img/icon/calendar.png" alt="">{{date_format($blog->created_at, 'd F Y')}}</span>
+                            <h5>{{$blog->title}}</h5>
+                            <a href="{{route('blog.detail.index', ['blog' => $blog->id])}}">Read More</a>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-lg-4 col-md-6 col-sm-6">
-                <div class="blog__item">
-                    <div class="blog__item__pic set-bg" data-setbg="front/img/blog/blog-2.jpg"></div>
-                    <div class="blog__item__text">
-                        <span><img src="front/img/icon/calendar.png" alt=""> 21 February 2020</span>
-                        <h5>Eternity Bands Do Last Forever</h5>
-                        <a href="#">Read More</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 col-sm-6">
-                <div class="blog__item">
-                    <div class="blog__item__pic set-bg" data-setbg="front/img/blog/blog-3.jpg"></div>
-                    <div class="blog__item__text">
-                        <span><img src="front/img/icon/calendar.png" alt=""> 28 February 2020</span>
-                        <h5>The Health Benefits Of Sunglasses</h5>
-                        <a href="#">Read More</a>
-                    </div>
-                </div>
-            </div>
+            @endforeach
         </div>
     </div>
 </section>
